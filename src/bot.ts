12978793f60d1db24af16a19522488a68d335b07
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-10-25 11:59:52 +0300

    Update intents

--- src/bot.ts
+++ src/bot.ts
@@ -7,10 +7,19 @@ const TOKEN = process.env.DISCORD_BOT_TOKEN;
 
 const client: Client = new Client({
 	intents: [
-		IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.AutoModerationExecution,
     IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildBans,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildInvites,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildWebhooks,
+    IntentsBitField.Flags.GuildPresences,
+    IntentsBitField.Flags.GuildVoiceStates,
+    IntentsBitField.Flags.GuildMessageReactions,
+    IntentsBitField.Flags.DirectMessages,
+    IntentsBitField.Flags.DirectMessageReactions,
 	]
 });
 	client.login(TOKEN);
