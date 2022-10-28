117bc9a41196de61b40b2a3972ec027299efd40a
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-10-28 21:23:18 +0300

    Choice sketches of the command to create a button

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,34 +1,50 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, } from 'discord.js';
+import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonStyle, Events, InteractionType } from 'discord.js';
 import { CommandObject, CommandType } from "wokcommands";
 
 export default {
   description: 'Button to add a role.',
   type: CommandType.SLASH,
 
-  minArgs: 1,
-  maxArgs: 1,
-  expectedArgs: "<role-name>",
+  minArgs: 3,
+  maxArgs: 3,
+  expectedArgs: "<role-name> <button-title> <button-style>",
+
+  options: [
+    {
+      name: 'role-name',
+      description: 'sf',
+      required: true,
+      type: ApplicationCommandOptionType.String,
+      choices: [
+        {name: 'one', value: '1'}
+      ]
+    }
+  ],
+  
 
   callback: async ({client, args, interaction}: any) => {
+    const [ role_name, button_title, button_style ] = args;
+
     interaction.reply({ components: [new ActionRowBuilder({
       components: [
         new ButtonBuilder({
-          customId: 'button-one',
-          label: 'Click me',
+          customId: `role-button-${role_name}`,
+          label: `${button_title}`,
           style: ButtonStyle.Success,
         })
       ]
-    })] 
-    });
+    })]});
 
-    client.on(Events.InteractionCreate, (interaction: any) => {
+    client.on(Events.InteractionCreate, async (interaction: any) => {
       if (!interaction.isButton()) return;
-      if (interaction.customId === 'button-one') {
-        let user = interaction.guild.members.cache.get(interaction.member.user.id);
-        let role = interaction.guild.roles.cache.find((role: any) => role.name === args[0]);
+      if (interaction.customId === `role-button-${role_name}`) {
+        const user = interaction.guild.members.cache.get(interaction.member.user.id);
+        const role = interaction.guild.roles.cache.find((role: any) => role.name === args[0]);
 
         user.roles.add(role);
       }
     });
+
+    return 
   }
 } as CommandObject;
