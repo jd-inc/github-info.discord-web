e4d70823b10d0d3789b4a088e5ef56c6549ca6c8
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-10-29 16:28:58 +0300

    Modal sketches for adding buttons with roles

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,25 +1,26 @@
-import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonStyle, Events, InteractionType } from 'discord.js';
+import { ModalBuilder, TextInputBuilder } from '@discordjs/builders';
+import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonStyle, Events, InteractionType, TextInputStyle } from 'discord.js';
 import { CommandObject, CommandType } from "wokcommands";
 
 export default {
   description: 'Button to add a role.',
   type: CommandType.SLASH,
 
-  minArgs: 3,
-  maxArgs: 3,
-  expectedArgs: "<role-name> <button-title> <button-style>",
+  minArgs: 1,
+  maxArgs: 1,
+  expectedArgs: "<role-name>"/* <button-title> <button-style>*/,
 
-  options: [
-    {
-      name: 'role-name',
-      description: 'sf',
-      required: true,
-      type: ApplicationCommandOptionType.String,
-      choices: [
-        {name: 'one', value: '1'}
-      ]
-    }
-  ],
+  // options: [
+  //   {
+  //     name: 'role-name',
+  //     description: 'sf',
+  //     required: true,
+  //     type: ApplicationCommandOptionType.String,
+  //     choices: [
+  //       {name: 'one', value: '1'}
+  //     ]
+  //   }
+  // ],
   
 
   callback: async ({client, args, interaction}: any) => {
@@ -35,16 +36,49 @@ export default {
       ]
     })]});
 
-    client.on(Events.InteractionCreate, async (interaction: any) => {
-      if (!interaction.isButton()) return;
-      if (interaction.customId === `role-button-${role_name}`) {
-        const user = interaction.guild.members.cache.get(interaction.member.user.id);
-        const role = interaction.guild.roles.cache.find((role: any) => role.name === args[0]);
+    const modal = new ModalBuilder()
+      .setCustomId('myModal')
+      .setTitle('My Modal');
 
-        user.roles.add(role);
-      }
+    // Add components to modal
+
+    // Create the text input components
+    // for (let i = 0; i < args[0]; i++) {
+    const favoriteColorInput = new TextInputBuilder()
+      .setCustomId('favoriteColorInput')
+        // The label is the prompt the user sees for this input
+      .setLabel("What's your favorite color?")
+        // Short means only a single line of text
+      .setStyle(TextInputStyle.Short);
+
+      const favoriteColor = new TextInputBuilder()
+      .setCustomId('favoriteColor')
+        // The label is the prompt the user sees for this input
+      .setLabel("What's your favorite color?")
+        // Short means only a single line of text
+      .setStyle(TextInputStyle.Short);
+
+    // An action row only holds one text input,
+    // so you need one action row per text input.
+    const firstActionRow: any[] = [new ActionRowBuilder().addComponents(favoriteColorInput), new ActionRowBuilder().addComponents(favoriteColor)];
+    //     firstActionRow.push(new ActionRowBuilder().addComponents(favoriteColorInput))
+    //   }
+      modal.addComponents(...firstActionRow);
+      console.log(modal);
+      
+    // Add inputs to the modal
+    
+    client.on(Events.InteractionCreate, async (interaction: any) => {
+      //   if (!interaction.isButton()) return;
+      //   if (interaction.customId === `role-button-${role_name}`) {
+        //     const user = interaction.guild.members.cache.get(interaction.member.user.id);
+        //     const role = interaction.guild.roles.cache.find((role: any) => role.name === args[0]);
+        
+        //     user.roles.add(role);
+        //   }
+        await interaction.showModal(modal);
     });
 
-    return 
+    
   }
 } as CommandObject;
