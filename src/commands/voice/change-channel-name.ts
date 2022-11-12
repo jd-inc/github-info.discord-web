c4d3a69d5feb0be1cf70db63e269c516231cb28d
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-11-12 22:03:37 +0300

    Added the ability to `change the name` of your `voice channel`.

--- src/commands/voice/change-channel-name.ts
+++ src/commands/voice/change-channel-name.ts
@@ -2,63 +2,63 @@ import { ApplicationCommandOptionType, ApplicationCommandType, VoiceChannel } fr
 import { SlashCommand } from "../../structures/Command";
 
 export default new SlashCommand({
-  name: 'invite-user',
+  name: 'change-channel-name',
   defaultMemberPermissions: 'Connect',
-  description: 'Пригласить в ваш канал другого пользователя.',
+  description: 'Изменить имя вашего канала.',
   type: ApplicationCommandType.ChatInput,
 
   options: [
     {
-      name: "user",
+      name: "text",
       description: 'Выберите пользователя.',
       required: true,
-      type: ApplicationCommandOptionType.User
+      type: ApplicationCommandOptionType.String,
+      maxLength: 22
     }
   ],
   
   run: async ({ interaction, client }) => {
     const workingChannel = interaction.guild.channels.cache.find(channel => channel.name === `voice-control`);
     const currentChannel = interaction.channel
-
+    
     if(currentChannel !== workingChannel) {
       interaction.reply({
         content: `Вводить эту команду можно только в канале ${workingChannel}`,
         ephemeral: true
       })
-
+      
       return;
     }
-
+    
     const voiceChannel = interaction.member.voice.channel;
-    const ownedChannel = client. voiceGenerator.get(interaction.member.id)
-    const targetUser = interaction.options.getUser("user");
+    const ownedChannel = client. voiceGenerator.get(interaction.member.id);
     
     if(!voiceChannel) {
       interaction.reply({
         content: `Вы не находитесь в голосовм канале!`,
         ephemeral: true
       })
-
+      
       return;
     }
-
+    
     if(!ownedChannel || voiceChannel.id !== ownedChannel){
       interaction.reply({
         content: `Вы не являетесь создателем этого канала!`,
         ephemeral: true
       })
-
+      
       return;
     }
     
-    voiceChannel.permissionOverwrites.edit(targetUser, {Connect: true});
-
-    targetUser.send({
-      content: `Пользователь ${interaction.member} приглашает вас в закрытый канал: ${voiceChannel}.`
-    })
+    const newName = interaction.options.get('text').value;
+    voiceChannel.edit({name: `${newName}`});
 
     interaction.reply({
-      content: `Пользователь ${targetUser} был приглашен в канал ${voiceChannel}.`
+      content: `Имя канала изменено на: ${newName}.`,
+      ephemeral: true
     })
+
+    return
   }
 })
