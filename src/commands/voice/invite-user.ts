f9bed33ec8fcfd64fd9025ea8da81632dd4e1dba
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-11-12 21:48:43 +0300

    Now `commands related to voice chat` can only be entered in a `special channel`.

--- src/commands/voice/invite-user.ts
+++ src/commands/voice/invite-user.ts
@@ -16,18 +16,41 @@ export default new SlashCommand({
     }
   ],
   
-  run: async ({ interaction }) => {
+  run: async ({ interaction, client }) => {
+    const workingChannel = interaction.guild.channels.cache.find(channel => channel.name === `voice-control`);
+    const currentChannel = interaction.channel
+
+    if(currentChannel !== workingChannel) {
+      interaction.reply({
+        content: `Вводить эту команду можно только в канале ${workingChannel}`,
+        ephemeral: true
+      })
+
+      return;
+    }
+
     const voiceChannel = interaction.member.voice.channel;
+    const ownedChannel = client. voiceGenerator.get(interaction.member.id)
+    const targetUser = interaction.options.getUser("user");
+    
     if(!voiceChannel) {
       interaction.reply({
         content: `Вы не находитесь в голосовм канале!`,
         ephemeral: true
       })
-      
+
+      return;
+    }
+
+    if(!ownedChannel || voiceChannel.id !== ownedChannel){
+      interaction.reply({
+        content: `Вы не являетесь создателем этого канала!`,
+        ephemeral: true
+      })
+
       return;
     }
     
-    const targetUser = interaction.options.getUser("user");
     voiceChannel.permissionOverwrites.edit(targetUser, {Connect: true});
 
     targetUser.send({
