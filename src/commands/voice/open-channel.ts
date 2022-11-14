a2e9a9092a27c2fb683c8103619a852374ed9997
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-11-14 17:31:37 +0300

    Added command to `open voice` for `everyone`.

---
+++ src/commands/voice/open-channel.ts
@@ -0,0 +1,42 @@
+import { ApplicationCommandType } from "discord.js";
+import { SlashCommand } from "../../structures/Command";
+
+export default new SlashCommand({
+  name: 'voice-open',
+  defaultMemberPermissions: 'Connect',
+  description: 'Открыть ваш голосовой канал для всех и каждого!',
+  type: ApplicationCommandType.ChatInput,
+  
+  run: async ({ interaction, client }) => {
+    const workingChannel = interaction.guild.channels.cache.find(channel => channel.name === `voice-control`);
+    const currentChannel = interaction.channel
+
+    if(currentChannel !== workingChannel) {
+      interaction.reply({
+        content: `Вводить эту команду можно только в канале ${workingChannel}`,
+        ephemeral: true
+      })
+
+      return;
+    }
+
+    const voiceChannel = interaction.member.voice.channel;
+    const ownedChannel = client. voiceGenerator.get(interaction.member.id)
+    const guildId = interaction.guildId
+    
+    if(!voiceChannel) {
+      interaction.reply({
+        content: `Вы не находитесь в голосовм канале!`,
+        ephemeral: true
+      })
+
+      return;
+    }
+    
+    voiceChannel.permissionOverwrites.edit(guildId, {Connect: true});
+
+    interaction.reply({
+      content: `Теперь все могут зайти в канал ${voiceChannel}!`
+    })
+  }
+})
